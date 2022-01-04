244de46a06e30e69df3de26b834c753d6e807bab
Vhmarega <57923276+TorugoMarega@users.noreply.github.com>
2022-01-04 20:27:47 -0300

    Co-authored-by: grm1003 <grm1003@users.noreply.github.com>

---
+++ semana_1/js/localStorage.js
@@ -0,0 +1,23 @@
+let nomeprojeto = document.querySelector ('#nomeprojeto');
+let descricaoprojeto = document.querySelector('#descricaoprojeto');
+let editorCodigo = document.querySelector('#editorCodigo').textContent;
+let linguagemSeletor = document.querySelector('#liguagem');
+let cores = document.querySelector ('#cores');
+
+console.log(editorCodigo)
+
+function salvar(){
+        let listaProjeto = JSON.parse(localStorage.getItem('listaProjeto') || '[]')   
+        listaProjeto.push(
+            {
+                editorCodigoSalva: editorCodigo,
+                nomeProjetoSalva: nomeprojeto.value,
+                descricaoProjetoSalva: descricaoprojeto.value,
+                linguagemSalva: linguagem.value,
+                coresSalva: cores.value,
+            });
+            localStorage.setItem('listaProjeto', JSON.stringify(listaProjeto));
+} 
+
+/* const abacate = document.querySelector('melhorfrutaparafazervitamina')- Torugo lindo e gostoso */
+
