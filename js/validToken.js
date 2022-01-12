1e5efff6b2b0a08c965a1c60f968a199950215a1
Vhmarega <57923276+TorugoMarega@users.noreply.github.com>
2022-01-11 21:35:38 -0300

    alteração dos caminhos de todos os arquivos

--- js/validToken.js
+++ js/validToken.js
@@ -1,12 +1,12 @@
 //-------------------------------------------ENCERRAR SESSÃO-------------------------------------------------
 let token = localStorage.getItem('token')
 
-if (localStorage.getItem('token') == null){
+if (localStorage.getItem('token') == null) {
     alert('Você precisa logar para continuar');
     window.location.href = '../index.html';
 }
 
-function sair(){
+function sair() {
     localStorage.removeItem('token');
-    window.location.href ='../index.html';
+    window.location.href = '../index.html';
 }
